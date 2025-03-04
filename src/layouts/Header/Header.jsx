749e4fadef22b3815b61d031c888ea9d1c4981ff
StasGoryachev <goriachev07@mail.ru>
2025-03-04 17:48:24 +0300

    logo

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,5 +1,13 @@
+import Logo from '@/components'
+import './Header.scss'
+
+
 const Header = () => {
-  return <header>Header</header>
+  return <header className="header">
+   <div className="header__inner container">
+      <Logo/>
+   </div>
+  </header>
 }
 
 export default Header
