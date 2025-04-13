5eabcfb25c3e910659216c60e37aef7f25a66a92
Stas <you@example.com>
2025-04-13 20:53:27 +0300

    hero+slider

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,26 @@
+import 'swiper/css'
+import './Slider.scss'
+import SliderNavigation from './components/SliderNavigation'
+
+const Slider = (props) => {
+  const {
+    children,
+    navigationTargetElementId = null,
+  } = props
+
+  return (
+    <div className="slider">
+      <div className="slider__swiper swiper">
+        <ul className="slider__list swiper-wrapper">
+          {children.map((slide, index) => (
+            <li className="slider__item swiper-slide" key={index}>
+              {slide}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Slider
---
+++ src/sections/Categories/Categories.jsx
@@ -0,0 +1,73 @@
+import Section from "@/layouts/Section"
+import CategoryCard from '@/components/CategoryCard'
+
+const Categories = () => {
+  const categoryItems = [
+    {
+      title: "Action",
+      images: [
+        "/src/assets/images/categories/action/1.jpg",
+        "/src/assets/images/categories/action/2.jpg",
+        "/src/assets/images/categories/action/3.jpg",
+        "/src/assets/images/categories/action/4.jpg",
+      ],
+    },
+    {
+      title: "Adventure",
+      images: [
+         '/src/assets/images/categories/action/5.jpg',
+         '/src/assets/images/categories/action/6.jpg',
+         '/src/assets/images/categories/action/7.jpg',
+         '/src/assets/images/categories/action/8.jpg',
+      ],
+    },
+    {
+      title: "Comedy",
+      images: [
+        "/src/assets/images/categories/action/1.jpg",
+        "/src/assets/images/categories/action/2.jpg",
+        "/src/assets/images/categories/action/3.jpg",
+        "/src/assets/images/categories/action/4.jpg",
+      ],
+    },
+    {
+      title: "Drama",
+      images: [
+        "/src/assets/images/categories/action/1.jpg",
+        "/src/assets/images/categories/action/2.jpg",
+        "/src/assets/images/categories/action/3.jpg",
+        "/src/assets/images/categories/action/4.jpg",
+      ],
+    },
+    {
+      title: "Horror",
+      images: [
+        "/src/assets/images/categories/action/1.jpg",
+        "/src/assets/images/categories/action/2.jpg",
+        "/src/assets/images/categories/action/3.jpg",
+        "/src/assets/images/categories/action/4.jpg",
+      ],
+    },
+  ]
+
+  return (
+    <Section
+      title="Explore our wide variety of categories"
+      titleId="categories-title"
+      description="Whether you're looking for a comedy to make you laugh, a drama to make you think, or a documentary to learn something new"
+      actions={
+        <div>
+          <button>Назад</button>
+          <button>Вперед</button>
+        </div>
+      }
+      isActionsHiddenOnMobile
+    >
+      {categoryItems.map((categoryItem, index) => (
+        <CategoryCard {...categoryItem} key={index} />
+      ))}
+    </Section>
+  )
+}
+
+export default Categories
